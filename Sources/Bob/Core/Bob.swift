2aa848cfff9a7bc04ef4719ceb71063eb6db8bfe
Ivan Damjanovic <ivan.damjanovic@number26.de>
2018-04-13 15:55:16 +0200

    [Bob] Increases version

--- Sources/Bob/Core/Bob.swift
+++ Sources/Bob/Core/Bob.swift
@@ -22,7 +22,7 @@ import Vapor
 
 public class Bob {
     
-    static let version: String = "1.0.2"
+    static let version: String = "1.1.0"
     
     /// Struct containing all properties needed for Bob to function
     public struct Configuration {
