76cb0760b472793ac482bea2828915fe613986e2
Kent Humphries <kenthumphries@users.noreply.github.com>
2018-10-02 10:34:16 +0200

    [Bob] Bumps version number.

--- Sources/Bob/Core/Bob.swift
+++ Sources/Bob/Core/Bob.swift
@@ -22,7 +22,7 @@ import Vapor
 
 public class Bob {
     
-    static let version: String = "1.2.2"
+    static let version: String = "1.3.0"
     
     /// Struct containing all properties needed for Bob to function
     public struct Configuration {
