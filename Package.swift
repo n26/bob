eb7b373bdad1e92bd09cd11d970e2bb99e938474
Jan Chaloupecky <jan.chaloupecky@n26.com>
2019-07-31 16:07:11 +0200

    updated to swift-tools-version:4.2

--- Package.swift
+++ Package.swift
@@ -1,11 +1,16 @@
+// swift-tools-version:4.2
+
 import PackageDescription
 
 let package = Package(
     name: "Bob",
-    targets: [
-        Target(name: "BobMain", dependencies: ["Bob"]),
+    products: [
+        .library(name: "Bob", targets: ["Bob"]),
     ],
     dependencies: [
-    .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2, minor: 0),
+        .package(url: "https://github.com/vapor/vapor.git", from: "2.0.0"),
+    ],
+    targets: [
+        .target(name: "Bob", dependencies: ["Vapor"]),
     ]
 )
