ce0c7b701e0a5f2ebf646892776849a37ad47f6d
Jan Chaloupecky <jan.chaloupecky@n26.com>
2019-12-02 15:07:10 +0100

    fixup! [Bob] Added info command

--- Sources/Bob/Commands/Info/Info Providers/InfoProvider.swift
+++ Sources/Bob/Commands/Info/Info Providers/InfoProvider.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// Used by the `InfoCommand` to provide a name and value to the list
-/// Use you can register a provider infoCommand.register(MyInfoProvider())
+/// Use you can register a provider using `infoCommand.register(MyInfoProvider())`
 public protocol InfoProvider {
     /// The name of the provided information printed to Slack
     var name: String { get }
